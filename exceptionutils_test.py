7b8489c051e101261c21d7c104ea43d1ec4b7153
Ryan Gabbard <gabbard@isi.edu>
2018-04-11 14:22:20 -0400

    Add check_in precondition

--- exceptionutils_test.py
+++ exceptionutils_test.py
@@ -1,14 +1,16 @@
 from unittest import TestCase
 
+import flexnlp.utils.misc_flexnlp_utils
+import flexnlp.utils.misc_utils
 from flexnlp.annotators.util import exceptionutils
 
 
 class ExceptionUtilsTest(TestCase):
     def test_limit_str(self):
         self.assertEqual("['foo', 'bar', 'baz' and 3 more]",
-                         exceptionutils.str_list_limited(['foo', 'bar', 'baz', 'moo', 'oink',
+                         flexnlp.utils.misc_utils.str_list_limited(['foo', 'bar', 'baz', 'moo', 'oink',
                                                           'baa'], limit=3))
         self.assertEqual("['foo', 'bar', 'baz']",
-                         exceptionutils.str_list_limited(['foo', 'bar', 'baz'], limit=3))
+                         flexnlp.utils.misc_utils.str_list_limited(['foo', 'bar', 'baz'], limit=3))
         self.assertEqual("['foo', 'bar', 'baz' and 1 more]",
-                         exceptionutils.str_list_limited(['foo', 'bar', 'baz', 'moo'], limit=3))
+                         flexnlp.utils.misc_utils.str_list_limited(['foo', 'bar', 'baz', 'moo'], limit=3))
