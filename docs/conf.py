95d378e93bb1a7a974647b23be72864fab383efc
Ryan Gabbard <gabbard@isi.edu>
2018-10-15 10:32:18 -0400

    Use 'any' as the default Sphinx role

--- docs/conf.py
+++ docs/conf.py
@@ -72,6 +72,9 @@ language = None
 # This patterns also effect to html_static_path and html_extra_path
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
+# text in backticks will be processed this way
+default_role = 'any'
+
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = 'sphinx'
 
