bb21cfdf971598f8d331efde45e37b13fdb80193
Ryan Gabbard <gabbard@isi.edu>
2019-02-04 16:57:42 -0500

    immutableset() not ready for prime time

--- tests/test_span.py
+++ tests/test_span.py
@@ -1,7 +1,7 @@
 from unittest import TestCase
 
 from attr import attrs, attrib
-from immutablecollections import immutableset
+from immutablecollections import ImmutableSet
 
 from vistautils.span import Span, HasSpanIndex
 
@@ -15,7 +15,9 @@ class TestSpan(TestCase):
     def test_index(self):
         items = Foo(Span(0, 10)), Foo(Span(5, 25)), Foo(Span(20, 30))
         index = HasSpanIndex.index(items)
-        self.assertEqual(immutableset([items[0]]), index.exactly_matching(Span(0, 10)))
-        self.assertEqual(immutableset([items[1]]), index.exactly_matching(Span(5, 25)))
-        self.assertEqual(immutableset([items[2]]), index.exactly_matching(Span(20, 30)))
-        self.assertEqual(immutableset(), index.exactly_matching(Span(6, 26)))
+        self.assertEqual(ImmutableSet.of([items[0]]), index.exactly_matching(Span(0, 10)))
+        self.assertEqual(ImmutableSet.of([items[1]]), index.exactly_matching(Span(5, 25)))
+        self.assertEqual(
+            ImmutableSet.of([items[2]]), index.exactly_matching(Span(20, 30))
+        )
+        self.assertEqual(ImmutableSet.empty(), index.exactly_matching(Span(6, 26)))
