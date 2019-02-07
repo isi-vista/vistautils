b12e03ea00c3e9851536ebc62fa157f594914b88
Ryan Gabbard <gabbard@isi.edu>
2019-02-07 12:53:39 -0500

    Fix so tests run cleanly with immutablecollections 0.5.0

--- vistautils/span.py
+++ vistautils/span.py
@@ -1,9 +1,8 @@
 from typing import Iterable, Optional, Sized, Tuple, TypeVar
 from typing_extensions import Protocol
 
-from attr import attrs
-from immutablecollections import ImmutableSet
-from immutablecollections.immutablemultidict import ImmutableSetMultiDict
+from attr import attrs, attrib
+from immutablecollections import ImmutableSet, ImmutableSetMultiDict, immutablesetmultidict
 
 from vistautils.attrutils import attrib_instance_of, attrib_immutable
 from vistautils.preconditions import check_arg
@@ -160,7 +159,7 @@ class HasSpanIndex(Protocol[T]):
         Creates a ``HasSpanIndex`` for the given items.
         """
         return _OverLappingHasSpanIndex(
-            ImmutableSetMultiDict.of(((item.span, item) for item in items))
+            immutablesetmultidict(((item.span, item) for item in items))
         )
 
 
@@ -170,9 +169,8 @@ class _OverLappingHasSpanIndex(HasSpanIndex[T]):
     An implementation of ``HasSpanIndex`` for items whose spans may overlap.
     """
 
-    _span_to_item_index: ImmutableSetMultiDict[Span, T] = attrib_immutable(
-        ImmutableSetMultiDict
-    )
+    _span_to_item_index: ImmutableSetMultiDict[Span, T] = attrib(
+        converter=immutablesetmultidict, default=immutablesetmultidict())
 
     def exactly_matching(self, span: Span) -> ImmutableSet[T]:
         return self._span_to_item_index[span]
