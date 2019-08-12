e2e080fb9d66ae0e27423d2c0dad3904d3489861
Ryan Gabbard <gabbard@isi.edu>
2019-08-12 12:01:58 -0400

    Consolidate stringification of parameters

--- vistautils/parameters_only_entrypoint.py
+++ vistautils/parameters_only_entrypoint.py
@@ -12,14 +12,6 @@ from vistautils.logging_utils import configure_logging_from
 
 log = logging.getLogger(__name__)  # pylint:disable=invalid-name
 
-
-def parameters_representer(dumper: yaml.Dumper, params: Parameters) -> yaml.Node:
-    """
-    PyYAML representer for Parameters objects.
-    """
-    return dumper.represent_mapping("tag:yaml.org,2002:map", params.as_mapping())
-
-
 def parameters_only_entry_point(
     main_method: Callable[[Parameters], None], usage_message: str = None
 ) -> None:
@@ -35,9 +27,7 @@ def parameters_only_entry_point(
     if len(sys.argv) == 2:
         params = YAMLParametersLoader().load(sys.argv[1])
         configure_logging_from(params)
-        yaml.add_representer(Parameters, parameters_representer)
-        parameters_as_yaml = yaml.dump(params, default_flow_style=False)
-        log.info("Ran with parameters:\n%s", parameters_as_yaml)
+        log.info("Ran with parameters:\n%s", params)
         main_method(params)
     else:
         if not usage_message:
