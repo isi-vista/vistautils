2577368fbfff04072e0fab057d166d3d8f888d8f
Ryan Gabbard <gabbard@isi.edu>
2019-10-01 14:26:54 -0400

    Guard against long exception messages in get_only.
    Also against Iterables which can't be iterated multiple times.

--- vistautils/collection_utils.py
+++ vistautils/collection_utils.py
@@ -1,4 +1,6 @@
-from typing import Iterable, TypeVar
+from typing import Iterable, Sized, TypeVar
+
+from vistautils.misc_utils import str_list_limited
 
 T = TypeVar("T")
 
@@ -9,17 +11,20 @@ def get_only(seq: Iterable[T]) -> T:
     """
     it = iter(seq)
     try:
-        val = it.__next__()
+        first_element = it.__next__()
         # we use the sentinel approach rather than the usual (evil) Python "attempt can catch the
         # exception" approach to avoid raising zillions of spurious exceptions on the expected
         # code path, which makes debugging a pain
         sentinel = object()
         second_element = next(it, sentinel)
         if second_element is sentinel:
-            return val
+            return first_element
         else:
-            raise ValueError(
-                "Expected one item in sequence but got multiple: %r" % (seq,)
-            )
+            got_msg: str
+            if isinstance(seq, Sized):
+                got_msg = str_list_limited(seq, limit=10)
+            else:
+                got_msg = f"{first_element!r}, {second_element!r}, and possibly more."
+            raise ValueError(f"Expected one item in sequence but got {got_msg}")
     except StopIteration:
         raise ValueError("Expected one item in sequence but got none")
