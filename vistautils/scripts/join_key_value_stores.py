b3dc78693edbd3a73943a97a483cfca68793c2c9
Ryan Gabbard <gabbard@isi.edu>
2020-02-28 15:19:33 -0800

    join_key_value_stores should both read and write in binary mode

--- vistautils/scripts/join_key_value_stores.py
+++ vistautils/scripts/join_key_value_stores.py
@@ -1,21 +1,24 @@
 #!/usr/bin/env python
 
 """
-Joins multiple character key-value stores into one.
+Joins multiple key-value stores into one.
 
 This is useful when recombining the results of a corpus which has been split for parallel
 processing.
 
-Currently the input stores are always assumed to be standard zip file stores. This might become
-configurable in the future.
+Currently the input stores are always assumed to be standard zip file stores.
+This might become configurable in the future.
 
-The "input_store_list_file" parameter points to a file containing the paths of the stores to join.
-The "output" namespace describes the key-value store to output to. See
-char_key_value_sink_from_params for details.
+The *input_store_list_file* parameter points to a file containing the paths of the stores to join.
+The *output* namespace describes the key-value store to output to.
+See `byte_key_value_sink_from_params` for details.
 """
 from typing import Set
 
-from vistautils.key_value import KeyValueSource, char_key_value_sink_from_params
+from vistautils.key_value import (
+    KeyValueSource,
+    byte_key_value_sink_from_params,
+)
 from vistautils.parameters import Parameters
 from vistautils.parameters_only_entrypoint import parameters_only_entry_point
 
@@ -25,7 +28,7 @@ def main(params: Parameters):
         "input_store_list_file", log_name="input key-value stores"
     )
     keys_written: Set[str] = set()
-    with char_key_value_sink_from_params(params, eval_context=locals()) as out:
+    with byte_key_value_sink_from_params(params, eval_context=locals()) as out:
         for input_path in input_paths:
             with KeyValueSource.zip_bytes_source(input_path) as inp:
                 for key in inp.keys():
