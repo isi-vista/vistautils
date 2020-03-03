b2dcb20fd98bdeff4cae5122513ce6efb717d17c
Justin Martin <professional@justinm.me>
2020-03-03 12:55:53 +0000

    Split tests to be more readable
    - Added testing of duplicate keys and different size splits in main test

--- tests/scripts/test_split_key_value_store.py
+++ tests/scripts/test_split_key_value_store.py
@@ -15,6 +15,28 @@ def test_split_key_value_store_explicit_split(tmp_path: Path):
     foo_params = Parameters.from_mapping(
         {"output_file": str(output_foo), "keys_file": str(keys_foo)}
     )
+    foo_reference = {"key1": "value1", "key2": "value2"}
+
+    output_bar = tmp_path / "bar"
+    keys_bar = tmp_path / "bar_keys"
+    with keys_bar.open("w") as keys:
+        keys.write("key3\nkey4\nkey2")
+    bar_params = Parameters.from_mapping(
+        {"output_file": str(output_bar), "keys_file": str(keys_bar)}
+    )
+    bar_reference = {"key2": "value2", "key3": "value3", "key4": "value4"}
+
+    # Testing handling of empty key file
+    output_none = tmp_path / "none"
+    keys_none = tmp_path / "no_keys"
+    keys_none.touch()
+    none_params = Parameters.from_mapping(
+        {"output_file": str(output_none), "keys_file": str(keys_none)}
+    )
+
+    split_params = Parameters.from_mapping(
+        {"foo": foo_params, "bar": bar_params, "none": none_params}
+    )
 
     key_value_path = tmp_path / "key_value"
     with KeyValueSink.zip_character_sink(key_value_path) as sink:
@@ -25,7 +47,45 @@ def test_split_key_value_store_explicit_split(tmp_path: Path):
 
     input_params = Parameters.from_mapping({"type": "zip", "path": str(key_value_path)})
 
-    non_exhaustive_disallowed = Parameters.from_mapping(
+    final_params = Parameters.from_mapping(
+        {"input": input_params, "explicit_split": split_params}
+    )
+
+    split_key_value_store.main(final_params)
+
+    with KeyValueSource.zip_character_source(output_foo) as source:
+        assert set(source.keys()) == set(foo_reference.keys())
+        for key, reference_value in foo_reference.items():
+            assert source[key] == reference_value
+
+    with KeyValueSource.zip_character_source(output_bar) as source:
+        assert set(source.keys()) == set(bar_reference.keys())
+        for key, reference_value in bar_reference.items():
+            assert source[key] == reference_value
+
+    with KeyValueSource.zip_character_source(output_none) as source:
+        assert set(source.keys()) == set()
+
+
+def test_split_key_value_store_explicit_split_non_exhaustive_disallowed(tmp_path: Path):
+    output = tmp_path / "foo"
+    keys = tmp_path / "foo_keys"
+    with keys.open("w") as kf:
+        kf.write("key1\nkey2")
+    foo_params = Parameters.from_mapping(
+        {"output_file": str(output), "keys_file": str(keys)}
+    )
+
+    key_value_path = tmp_path / "key_value"
+    with KeyValueSink.zip_character_sink(key_value_path) as sink:
+        sink.put("key1", "value1")
+        sink.put("key2", "value2")
+        sink.put("key3", "value3")
+        sink.put("key4", "value4")
+
+    input_params = Parameters.from_mapping({"type": "zip", "path": str(key_value_path)})
+
+    final_params = Parameters.from_mapping(
         {
             "input": input_params,
             "explicit_split": Parameters.from_mapping({"foo": foo_params}),
@@ -39,11 +99,28 @@ def test_split_key_value_store_explicit_split(tmp_path: Path):
             "parameter must_be_exhaustive to False"
         ),
     ):
-        split_key_value_store.main(non_exhaustive)
+        split_key_value_store.main(final_params)
 
-    output_foo.unlink()
 
-    non_exhaustive_allowed = Parameters.from_mapping(
+def test_split_key_value_store_explicit_split_non_exhaustive_allowed(tmp_path: Path):
+    output = tmp_path / "foo"
+    keys = tmp_path / "foo_keys"
+    with keys.open("w") as kf:
+        kf.write("key1\nkey2")
+    foo_params = Parameters.from_mapping(
+        {"output_file": str(output), "keys_file": str(keys)}
+    )
+
+    key_value_path = tmp_path / "key_value"
+    with KeyValueSink.zip_character_sink(key_value_path) as sink:
+        sink.put("key1", "value1")
+        sink.put("key2", "value2")
+        sink.put("key3", "value3")
+        sink.put("key4", "value4")
+
+    input_params = Parameters.from_mapping({"type": "zip", "path": str(key_value_path)})
+
+    final_params = Parameters.from_mapping(
         {
             "input": input_params,
             "explicit_split": Parameters.from_mapping({"foo": foo_params}),
@@ -51,56 +128,11 @@ def test_split_key_value_store_explicit_split(tmp_path: Path):
         }
     )
 
-    split_key_value_store.main(non_exhaustive)
-
-    foo_reference = {"key1": "value1", "key2": "value2"}
-
-    with KeyValueSource.zip_character_source(output_foo) as source:
-        assert set(source.keys()) == set(foo_reference.keys())
-        for key, reference_value in foo_reference.items():
-            assert source[key] == reference_value
-
-    output_foo.unlink()
-
-    # Testing full exhaustive script and handling of an empty keys file
-    output_bar = tmp_path / "bar"
-    keys_bar = tmp_path / "bar_keys"
-    with keys_bar.open("w") as keys:
-        keys.write("key3\nkey4")
-    bar_params = Parameters.from_mapping(
-        {"output_file": str(output_bar), "keys_file": str(keys_bar)}
-    )
-
-    output_none = tmp_path / "none"
-    keys_none = tmp_path / "no_keys"
-    keys_none.touch()
-    none_params = Parameters.from_mapping(
-        {"output_file": str(output_none), "keys_file": str(keys_none)}
-    )
-
-    split_params = Parameters.from_mapping(
-        {"foo": foo_params, "bar": bar_params, "none": none_params}
-    )
-
-    final_params = Parameters.from_mapping(
-        {"input": input_params, "explicit_split": split_params}
-    )
-
     split_key_value_store.main(final_params)
 
-    foo_reference = {"key1": "value1", "key2": "value2"}
-    bar_reference = {"key3": "value3", "key4": "value4"}
+    reference = {"key1": "value1", "key2": "value2"}
 
-    with KeyValueSource.zip_character_source(output_foo) as source:
-        assert set(source.keys()) == set(foo_reference.keys())
-        for key, reference_value in foo_reference.items():
+    with KeyValueSource.zip_character_source(output) as source:
+        assert set(source.keys()) == set(reference.keys())
+        for key, reference_value in reference.items():
             assert source[key] == reference_value
-
-    with KeyValueSource.zip_character_source(output_bar) as source:
-        assert set(source.keys()) == set(bar_reference.keys())
-        for key, reference_value in bar_reference.items():
-            assert source[key] == reference_value
-
-    # This fails currently - set(source.keys()) returns `{''}`
-    with KeyValueSource.zip_character_source(output_none) as source:
-        assert set(source.keys()) == set()
