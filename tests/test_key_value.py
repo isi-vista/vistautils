152f2716e39cca7e15ffd0d3940658665157ee60
Justin Martin <professional@justinm.me>
2020-03-06 13:26:33 +0000

    Using .get() to increase coverage and correcting tabbing on block

--- tests/test_key_value.py
+++ tests/test_key_value.py
@@ -203,14 +203,16 @@ def test_doc_id_from_file(tmp_path: Path) -> None:
         tmp_file.write("pong")
 
     with KeyValueSource.binary_from_doc_id_to_file_map(tmp_path / "example.tab") as sink:
-        assert sink["world"].decode("utf-8") == "hello"
-        assert sink["ping"].decode("utf-8") == "pong"
+        assert sink.get("world").decode("utf-8") == "hello"
+        assert sink.get("ping").decode("utf-8") == "pong"
+        assert sink.get("nonexistent") == None
 
     with KeyValueSource.binary_from_doc_id_to_file_map(
         str(tmp_path / "example.tab")
     ) as sink:
-        assert sink["world"].decode("utf-8") == "hello"
-        assert sink["ping"].decode("utf-8") == "pong"
+        assert sink.get("world").decode("utf-8") == "hello"
+        assert sink.get("ping").decode("utf-8") == "pong"
+        assert sink.get("nonexistent") == None
 
 
 def test_empty_zip_key_value(tmp_path: Path) -> None:
@@ -224,7 +226,7 @@ def test_empty_zip_key_value(tmp_path: Path) -> None:
         assert set(source.keys()) == set()
 
 
-def test_from_path_mapping(tmp_path: Path):
+def test_from_path_mapping_char(tmp_path: Path):
     value1 = tmp_path / "value1"
     with value1.open("w") as vf:
         vf.write("hello")
@@ -249,4 +251,4 @@ def test_from_path_mapping(tmp_path: Path):
         for (k, v) in reference.items():
             assert source.get(k) == v
 
-    assert source.get("Nonexistent") == None
+        assert source.get("Nonexistent") == None
